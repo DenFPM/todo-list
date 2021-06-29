5131be0ae8b0be4d83bbe8102bc62eac3733f5cc
KroJl_dev <brivka2017@gmail.com>
2021-06-29 22:49:17 +0300

    formatted

--- src/redux/actions/setTodoActions.js
+++ src/redux/actions/setTodoActions.js
@@ -2,10 +2,10 @@ export const setTodosData = (payload) => ({
   type: "SET_TODOS_DATA",
   payload,
 });
-export const changeTodosData = (todosArr,oldTodosArr) => ({
+export const changeTodosData = (todosArr, oldTodosArr) => ({
   type: "CHANGE_NEW_TODOS_DATA",
   todosArr,
-  oldTodosArr
+  oldTodosArr,
 });
 export const setIsOldTodos = () => ({
   type: "SET_IS_OLD_TODOS",
